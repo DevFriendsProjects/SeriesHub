6d7dd0d95a048bcdb71227f92a9ea3de25a69c8c
Matheus Ribeiro <matheus.ribeirors95@gmail.com>
2024-08-13 17:21:06 -0300

    Alteração na pagina home, adição das informações de séries e usuários cadastrados no sistema, alinhamento da navbar e alteração na fonte do projeto

--- resources/views/series/home.blade.php
+++ resources/views/series/home.blade.php
@@ -8,4 +8,16 @@
         <img class="img-home" src="https://cdn.colab55.com/images/55001/studio/75262/art/173249/stickers.png" alt="img"/>
     </div>
 
+    <div class="card-info">
+        <div class="series-cad">
+            <h3>Séries cadastradas</h3>
+            <p>4</p>
+        </div>
+
+        <div class="usuario-cad">
+            <h3>Usuários cadastrados</h3>
+            <p>1</p>
+        </div>
+    </div>
+
 </x-layout>
