3935c49c541fef2b06ab869faa78ef4196fc08e5
Matheus Ribeiro <matheus.ribeirors95@gmail.com>
2024-08-20 17:44:48 -0300

    ajuste no alinhamento do menu mobile e animação no titulo da parte de cadastro

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -10,9 +10,9 @@
         </div>
     @endif
 
-    <form action="{{ route('series.store') }}" method="post" enctype="multipart/form-data">
+    <form class="form-cadastro" action="{{ route('series.store') }}" method="post" enctype="multipart/form-data">
         @csrf
-        <h1 class="titulo-cadastro" class="text-center mt-4" style="color: pink; margin-right: 100px;">Adicione sua Série</h1>
+        <h1 class="titulo-cadastro" class="text-center mt-4" style="color: pink; margin-right: 100px; font-weight: bold">Adicione sua Série</h1>
         <input type="text" id="seriesInput" name="name" placeholder="Adicione o nome da série" required>
         <input type="number" id="episodesInput" name="seasons" placeholder="Adicione o número de temporadas" min="1" required>
         <button type="submit" class="btn btn-success">Adicionar</button>
